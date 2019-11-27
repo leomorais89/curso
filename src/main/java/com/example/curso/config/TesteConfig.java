dd8ab0bdedcf78ff76995c7b25be976d75bd3078
Leonardo Morais <leonardomorais89@yahoo.com.br>
2019-11-27 16:17:43 -0300

    Pagamento funcionando

--- src/main/java/com/example/curso/config/TesteConfig.java
+++ src/main/java/com/example/curso/config/TesteConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.curso.entidades.Categoria;
 import com.example.curso.entidades.ItemPedido;
+import com.example.curso.entidades.Pagamento;
 import com.example.curso.entidades.Pedido;
 import com.example.curso.entidades.Produto;
 import com.example.curso.entidades.Usuario;
@@ -48,6 +49,9 @@ public class TesteConfig implements CommandLineRunner {
 		Pedido o1 = new Pedido(null, Instant.parse("2019-06-20T19:53:07Z"), u1, StatusPedido.PAGO);
 		Pedido o2 = new Pedido(null, Instant.parse("2019-07-21T03:42:10Z"), u2, StatusPedido.ESPERANDO_PAGAMENTO);
 		Pedido o3 = new Pedido(null, Instant.parse("2019-07-22T15:21:22Z"), u1, StatusPedido.ESPERANDO_PAGAMENTO);
+		
+		Pagamento pagamento = new Pagamento(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPagamento(pagamento);
 		
 		usuarioRepositorio.saveAll(Arrays.asList(u1, u2));
 		pedidoRepositorio.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/example/curso/entidades/Pedido.java
+++ src/main/java/com/example/curso/entidades/Pedido.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,9 +13,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import com.example.curso.entidades.enuns.StatusPedido;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Pedido implements Serializable {
@@ -32,6 +33,9 @@ public class Pedido implements Serializable {
 	
 	@OneToMany(mappedBy = "id.pedido")
 	List<ItemPedido> itensPedido = new ArrayList<>();
+	
+	@OneToOne(mappedBy = "pedido", cascade = CascadeType.ALL)
+	private Pagamento pagamento;
 	
 	public Pedido() {
 		
@@ -81,6 +85,14 @@ public class Pedido implements Serializable {
 
 	public List<ItemPedido> getItensPedido() {
 		return itensPedido;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
 	}
 
 	@Override
