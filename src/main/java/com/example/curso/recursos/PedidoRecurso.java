827d4fcb367d734813707b374f29f5346ab61a42
Leonardo Morais <leonardomorais89@yahoo.com.br>
2019-11-27 21:16:18 -0300

    CRUD de pedido está feito, e gerando um pagamento

--- src/main/java/com/example/curso/recursos/PedidoRecurso.java
+++ src/main/java/com/example/curso/recursos/PedidoRecurso.java
@@ -1,13 +1,19 @@
 package com.example.curso.recursos;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.curso.entidades.Pedido;
 import com.example.curso.servicos.PedidoServico;
@@ -28,6 +34,25 @@ public class PedidoRecurso {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Pedido> buscaPorId(@PathVariable Integer id) {
 		Pedido pedido = servico.buscaPorId(id);
+		return ResponseEntity.ok().body(pedido);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Pedido> fazerPedido(@RequestBody Pedido pedido) {
+		pedido = servico.fazerPedido(pedido);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(pedido.getId()).toUri();
+		return ResponseEntity.created(uri).body(pedido);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> cancelar(@PathVariable Integer id) {
+		servico.cancelar(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Pedido> alterar(@PathVariable Integer id, @RequestBody Pedido pedido) {
+		pedido = servico.alterar(id, pedido);
 		return ResponseEntity.ok().body(pedido);
 	}
 }
--- src/main/java/com/example/curso/servicos/PedidoServico.java
+++ src/main/java/com/example/curso/servicos/PedidoServico.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.curso.entidades.ItemPedido;
+import com.example.curso.entidades.Pagamento;
 import com.example.curso.entidades.Pedido;
+import com.example.curso.repositorios.ItemPedidoRepositorio;
 import com.example.curso.repositorios.PedidoRepositorio;
 
 @Service
@@ -15,6 +18,9 @@ public class PedidoServico {
 	@Autowired
 	private PedidoRepositorio repositorio;
 	
+	@Autowired
+	private ItemPedidoRepositorio itemPedidoRepostitorio;
+	
 	public List<Pedido> buscaTodos() {
 		return repositorio.findAll();
 	}
@@ -22,5 +28,29 @@ public class PedidoServico {
 	public Pedido buscaPorId(Integer id) {
 		Optional<Pedido> pedido = repositorio.findById(id);
 		return pedido.get();
+	}
+	
+	public Pedido fazerPedido(Pedido pedido) {
+		List<ItemPedido> itensPedido = pedido.getItensPedido();
+		pedido = repositorio.save(pedido);
+		for (ItemPedido itens : itensPedido) {
+			itens.setPedido(pedido);
+		}
+		itemPedidoRepostitorio.saveAll(itensPedido);
+		return pedido;
+	}
+	
+	public void cancelar(Integer id) {
+		repositorio.deleteById(id);
+	}
+	
+	public Pedido alterar(Integer id, Pedido pedido) {
+		Pedido entidade = repositorio.getOne(id);
+		entidade.setStatus(pedido.getStatus());
+		if (pedido.getPagamento() != null) {
+			Pagamento pagamento = new Pagamento(null, pedido.getPagamento().getMomento(), pedido);
+			entidade.setPagamento(pagamento);
+		}
+		return repositorio.save(entidade);
 	}
 }
